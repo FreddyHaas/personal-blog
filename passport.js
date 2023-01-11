30058a3874a3699e4316ca438c50ad204e83b89e
Freddy Haas <email@frederik-haas.de>
2023-01-11 16:43:11 +0100

    Fix authentication upon login

--- passport.js
+++ passport.js
@@ -2,12 +2,13 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('./models/user')
 
-passport.use(new LocalStrategy({
+passport.use(
+    new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password'
     },
-    function (email, password, cb) {
-        return User.findOne({ email, password }, (err, user) => {
+    function (email, password, done) {
+        User.findOne({ email }, (err, user) => {
             if (err) {
                 return done (err)
             }
@@ -17,9 +18,7 @@ passport.use(new LocalStrategy({
             if (password != user.password)  {
                 return done(null, false, { message: 'Incorrect password' })
             }
-
             return done(null, user, { message: 'Logged In Successfully'})
         })
     }
-
 ))
--- routes/user.js
+++ routes/user.js
@@ -9,20 +9,24 @@ const userController = require('../controllers/userController')
 router.post('/signup', userController.signUp)
 
 // POST: login
-router.post('/login', (req, res) => {
-    
+router.post('/login', function (req, res, next) {
     // Authenticate user
-    passport.authenticate('local', { session: false }, (err, user) => {
+    passport.authenticate('local', { session: false }, (err, user, next) => {
         if (err || !user) {
             return res.status(400).json({
                 message: 'Not possible to login',
                 user: user
             })
         }
-        // Create JSON Webtoken
-        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
-        res.json({ accessToken: accessToken })
-    })
+        req.login(user, { session: false }, (err) => {
+            if (err) {
+                res.send(err)
+            }
+            // Create JSON Webtoken
+            const accessToken = jwt.sign({user}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            res.json({ accessToken: accessToken })
+        })
+    })(req, res)
 })
 
 module.exports = router
