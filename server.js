becc347d1ca7bdc89d8963f4e6237de28f25acf3
Freddy Haas <email@frederik-haas.de>
2023-01-12 13:26:45 +0100

    Set-up cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 require('./passport')
+const cors = require('cors')
 const app = express()
 const mongoose = require('mongoose')
 const passport = require('passport')
@@ -13,6 +14,10 @@ const db = mongoose.connection
 db.on('error', (error) => console.log(error))
 db.once('open', () => console.log('Connected to Database'))
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,   
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(passport.initialize())
@@ -20,4 +25,4 @@ app.use(passport.initialize())
 app.use('/user', userRouter)
 app.use('/posts', postsRouter)
 
-app.listen(3000, () => console.log('Server started on port 3000'))
+app.listen(3500, () => console.log('Server started on port 3500'))
